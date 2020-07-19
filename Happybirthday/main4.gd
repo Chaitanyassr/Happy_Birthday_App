a0f5c71dc3642b2d89fe4bbacaba4eec9b28e46e
CHAITANYA <Chaitanyas0101@gmail.com>
2020-07-20 01:23:44 +0530

    add

---
+++ Happybirthday/main4.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ui_right_pressed():
+	get_tree().change_scene("main5.tscn")
+
+
+func _on_ui_left_pressed():
+	get_tree().change_scene("main3.tscn")
+	pass # Replace with function body.
