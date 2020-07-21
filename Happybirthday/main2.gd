bdfda0972d771d00ac4cfa5f8b5f8eb3a01eb588
CHAITANYA <Chaitanyas0101@gmail.com>
2020-07-22 01:51:50 +0530

    added new code

--- Happybirthday/main2.gd
+++ Happybirthday/main2.gd
@@ -11,3 +11,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_VideoPlayer_finished():
+	get_tree().change_scene("main.tscn")
+	pass # Replace with function body.
